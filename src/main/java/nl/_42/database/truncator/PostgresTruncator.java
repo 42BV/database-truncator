3a66a977e993910d442f9d78730c9becb4416677
robert-bor <bor.robert@gmail.com>
2017-01-26 21:30:06 +0100

    #1 Also added sequence reset functionality for Postgres
    Also looked like the entire POM was missing from the git files. Fixed that.

--- src/main/java/nl/_42/database/truncator/PostgresTruncator.java
+++ src/main/java/nl/_42/database/truncator/PostgresTruncator.java
@@ -13,13 +13,16 @@ public class PostgresTruncator extends AbstractDatabaseTruncator {
     public static final String SQL_GET_TABLENAMES =         "SELECT * FROM pg_catalog.pg_tables";
     public static final String PUBLIC_SCHEMA =              "public";
     public static final String TABLE_NAME_KEY =             "tablename";
+    public static final String SEQUENCE_NAME_KEY =          "relname";
     public static final String SCHEMA_NAME_KEY =            "schemaname";
 
     private final List<String> tables = new ArrayList<>();
+    private final List<String> sequences = new ArrayList<>();
 
     public PostgresTruncator(DataSource dataSource) {
         super(dataSource);
         determineTables();
+        determineSequences();
     }
 
     private void determineTables() {
@@ -48,6 +51,13 @@ public class PostgresTruncator extends AbstractDatabaseTruncator {
                 .collect(Collectors.toList()));
     }
 
+    private void determineSequences() {
+        String query = "SELECT c.relname FROM pg_class c WHERE c.relkind = 'S'";
+        this.sequences.addAll(jdbcTemplate.queryForList(query).stream()
+                .map(sequenceInfo -> (String) sequenceInfo.get(SEQUENCE_NAME_KEY))
+                .collect(Collectors.toList()));
+    }
+
     private static boolean isTruncatableTable(String tableName) {
         return
                 !tableName.equals(LIQUIBASE_CHANGE_LOG) &&
@@ -56,6 +66,7 @@ public class PostgresTruncator extends AbstractDatabaseTruncator {
 
     public void truncate() throws MetaDataAccessException {
         tables.forEach(table -> jdbcTemplate.execute("SELECT truncate_tables('" + table + "')"));
+        sequences.forEach(sequence -> jdbcTemplate.execute("ALTER SEQUENCE " + sequence + " RESTART WITH 1"));
     }
 
 }
