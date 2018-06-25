6ef38788f398e55a7a481a2e7c0ede34ebe2c463
Arjan Vlek <arjan.vlek@42.nl>
2018-06-25 13:27:17 +0200

    Added another test for truncating table which is ignored in the config

--- src/test/java/nl/_42/database/truncator/DatabaseTruncatorIntegrationTest.java
+++ src/test/java/nl/_42/database/truncator/DatabaseTruncatorIntegrationTest.java
@@ -1,5 +1,7 @@
 package nl._42.database.truncator;
 
+import javax.sql.DataSource;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,31 +10,46 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.sql.DataSource;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfiguration.class)
 public class DatabaseTruncatorIntegrationTest {
 
-  @Autowired
-  private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-  @Autowired
-  private DatabaseTruncator truncator;
+    @Autowired
+    private DatabaseTruncator truncator;
 
-  @Test
-  public void testTruncate() throws Exception {
-    final JdbcTemplate template = new JdbcTemplate(dataSource);
+    private static final String MY_TABLE = "my_table";
+    private static final String IGNORED_TABLE = "ignored_table";
 
-    template.execute("INSERT INTO my_table (col1) values (1)");
-    Assert.assertEquals(1, getCount(template));
+    @Test
+    public void testTruncate() throws Exception {
+        final JdbcTemplate template = new JdbcTemplate(dataSource);
 
-    truncator.truncate();
-    Assert.assertEquals(0, getCount(template));
-  }
+        template.execute("INSERT INTO my_table (col1) values (1)");
+        Assert.assertEquals(1, getCount(template, MY_TABLE));
 
-  private int getCount(JdbcTemplate template) {
-    return template.queryForObject("SELECT COUNT(1) FROM my_table", Integer.class);
-  }
+        truncator.truncate();
+        Assert.assertEquals(0, getCount(template, MY_TABLE));
+    }
+
+    @Test
+    public void testTruncateOfExcludedTable_shouldNotTruncate() throws Exception {
+        // Table is excluded in src/test/resources/application.properties (database-truncator.exclude).
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.execute("INSERT INTO ignored_table (col1) values (42)");
+
+        Assert.assertEquals(1, getCount(jdbcTemplate, IGNORED_TABLE));
+
+        truncator.truncate();
+
+        Assert.assertEquals(1, getCount(jdbcTemplate, IGNORED_TABLE));
+    }
+
+    private int getCount(JdbcTemplate template, String tableName) {
+        return template.queryForObject("SELECT COUNT(*) FROM " + tableName, Integer.class);
+    }
 
 }
