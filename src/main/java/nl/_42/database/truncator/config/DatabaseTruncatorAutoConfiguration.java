14d5f29a2658d79958300e3de3abad4ed0ce8316
Robin de Boer <robin.de.boer@42.nl>
2021-05-21 16:48:55 +0200

    DatabaseTruncator is now upgraded to work with Spring Boot 2.5.0
    - In DatabaseTruncatorAutoConfiguration changed the
    spring.datasource.platform config option to the new
    spring.sql.init.platform config. This means that it will now only
    support Spring Boot 2.5.0 and up.

--- src/main/java/nl/_42/database/truncator/config/DatabaseTruncatorAutoConfiguration.java
+++ src/main/java/nl/_42/database/truncator/config/DatabaseTruncatorAutoConfiguration.java
@@ -1,7 +1,10 @@
 package nl._42.database.truncator.config;
 
+import javax.sql.DataSource;
+
 import nl._42.database.truncator.DatabaseTruncator;
 import nl._42.database.truncator.Platform;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -11,15 +14,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 @ConditionalOnBean(DataSource.class)
-@ConditionalOnProperty(value= "spring.datasource.platform", matchIfMissing = true)
+@ConditionalOnProperty(value = "spring.sql.init.platform", matchIfMissing = true)
 @AutoConfigureAfter({ DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EnableConfigurationProperties(DatabaseTruncatorProperties.class)
 public class DatabaseTruncatorAutoConfiguration {
@@ -33,22 +35,22 @@ public class DatabaseTruncatorAutoConfiguration {
 
         private final DataSource dataSource;
 
-        private final DataSourceProperties dataSourceProperties;
+        private final SqlInitializationProperties sqlInitializationProperties;
 
         private final DatabaseTruncatorProperties databaseTruncatorProperties;
 
         @SuppressWarnings("SpringJavaAutowiringInspection")
-        public DatabaseTruncatorConfiguration(DataSource dataSource, DataSourceProperties dataSourceProperties,
-                                              DatabaseTruncatorProperties databaseTruncatorProperties) {
+        public DatabaseTruncatorConfiguration(DataSource dataSource, SqlInitializationProperties sqlInitializationProperties,
+                DatabaseTruncatorProperties databaseTruncatorProperties) {
             LOGGER.info("Configuring DatabaseTruncator");
             this.dataSource = dataSource;
-            this.dataSourceProperties = dataSourceProperties;
+            this.sqlInitializationProperties = sqlInitializationProperties;
             this.databaseTruncatorProperties = databaseTruncatorProperties;
         }
 
         @Bean
         public DatabaseTruncator databaseTruncator() {
-            Platform platform = Platform.determinePlatform(dataSourceProperties.getPlatform());
+            Platform platform = Platform.determinePlatform(sqlInitializationProperties.getPlatform());
             return platform.createTruncator(dataSource, databaseTruncatorProperties);
         }
 
